215b9a1c5dbf951b2065f122a8328be872bafdae
Darknexus Dev <dev@darknexus.ai>
2026-03-29 20:46:18 +0200

    fix: supprimer ancien src/ racine (doublon de killagain-food/src)

--- src/pages/AssistantPage.jsx
+++
@@ -1,5 +0,0 @@
-import { AssistantPanel } from "../components/AssistantPanel";
-
-export function AssistantPage({ detectedIngredients }) {
-  return <AssistantPanel ingredients={detectedIngredients} />;
-}
--- src/pages/DietPage.jsx
+++
@@ -1,5 +0,0 @@
-import { DietPlanner } from "../components/DietPlanner";
-
-export function DietPage({ detectedIngredients }) {
-  return <DietPlanner ingredients={detectedIngredients} />;
-}
